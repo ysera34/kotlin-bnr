bc44f3e252296f944a0f6579d98ad5dfba5af152
ysera34 <ycyoon34@gmail.com>
2019-01-24 20:35:50 +0900

    chapter02 Variables, Constants, and Types: const val

--- chapter02/Sandbox/src/TypeIntro.kt
+++ chapter02/Sandbox/src/TypeIntro.kt
@@ -1,7 +1,9 @@
+const val MAX_EXPERIENCE = 5000
 fun main(args: Array<String>) {
     val playerName = "estragon"
     var experiencePoints = 5
     experiencePoints += 5
     println(playerName)
     println(experiencePoints)
+    println(MAX_EXPERIENCE)
 }
