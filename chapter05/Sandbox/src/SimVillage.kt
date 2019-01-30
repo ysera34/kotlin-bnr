4715232373884a08109e67ab10c01b689574c823
ysera34 <ycyoon34@gmail.com>
2019-01-31 00:25:15 +0900

    chapter05 Anonymous Functions and the Function Type: function type and arguments

--- chapter05/Sandbox/src/SimVillage.kt
+++ chapter05/Sandbox/src/SimVillage.kt
@@ -14,6 +14,11 @@ fun main(args: Array<String>) {
     }
     println(lettersNumber)
 
+    lettersNumber = "Mississippi".count {
+        it == 's'
+    }
+    println(lettersNumber)
+
 
     // () -> kotlin.String
     println({
@@ -25,4 +30,33 @@ fun main(args: Array<String>) {
         val currentYear = 2019
         "Welcome to SimVillage, (copyright $currentYear)"
     }())
+
+    val greetingFunction: () -> String = {
+        val currentYear = 2019
+        "Welcome to SimVillage, (copyright $currentYear)"
+    }
+    println(greetingFunction)
+    println(greetingFunction())
+
+    var greetingToPlayerFunction: (playerName: String) -> String = { playerName ->
+        val currentYear = 2019
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingToPlayerFunction)
+    println(greetingToPlayerFunction("james"))
+
+    greetingToPlayerFunction = { it ->
+        val currentYear = 2019
+        "Welcome to SimVillage, $it! (copyright $currentYear)"
+    }
+    println(greetingToPlayerFunction)
+    println(greetingToPlayerFunction("james"))
+
+    val greetingToPlayerAndPrintInfoFunction: (String, Int) -> String = { playerName, buildingsNumber ->
+        println("$buildingsNumber buildings have been added.")
+        val currentYear = 2019
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingToPlayerAndPrintInfoFunction)
+    println(greetingToPlayerAndPrintInfoFunction("james", 2))
 }
