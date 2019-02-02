bfef460ed8da8ec3c18cff38294f6c6a3fec8bc2
ysera34 <ycyoon34@gmail.com>
2019-02-02 13:57:44 +0900

    chapter06 Null Safety and Exceptions: null coalescing operator, elvis operator

--- chapter06/NyetHack/src/Tavern.kt
+++ chapter06/NyetHack/src/Tavern.kt
@@ -18,4 +18,23 @@ fun main(args: Array<String>) {
 //    beverage2 = null
 //    beverage2!!.capitalize() // Runtime Error: Exception in thread "main" kotlin.KotlinNullPointerException
     var beverage2 = readLine()!!.capitalize()
+
+    var beverage3 = readLine()
+    beverage3 = null
+
+    if (beverage3 != null) {
+        beverage3 = beverage3.capitalize() // Smart cast to kotlin.String
+    } else {
+        println("beverage is null")
+    }
+
+    println(beverage3)
+
+    val beverageServed: String = beverage3 ?: "Beer"
+    println(beverageServed)
+
+    var beverage4 = readLine()
+    beverage4?.let {
+        beverage4 = it.capitalize()
+    } ?: println("beverage is null")
 }
