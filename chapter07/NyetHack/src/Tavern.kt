a243196268589b8df4a62c97b0bafc6aa56e3d98
ysera34 <ycyoon34@gmail.com>
2019-02-02 16:25:42 +0900

    chapter07 Strings: replace strings, compare strings

--- chapter07/NyetHack/src/Tavern.kt
+++ chapter07/NyetHack/src/Tavern.kt
@@ -41,6 +41,7 @@ fun main(args: Array<String>) {
     } ?: println("beverage is null")
 
     placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
 
 private fun placeOrder(menuData: String) {
@@ -55,4 +56,26 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal purchases $name ($type) with $price gold coin(s)."
     println(message)
+
+    val phrase1 = "wow. $name is really good"
+    println("Madrigal admires. ${toDragonSpeak(phrase1)}")
+
+    val phrase2 = if (name == "Dragon's Breath") {
+        "Madrigal admires. ${toDragonSpeak("wow. $name is really good")}"
+    } else {
+        "Madrigal says. Thanks $name."
+    }
+    println(phrase2)
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
