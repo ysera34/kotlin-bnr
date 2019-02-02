cb9a037d978d1a983af66249dfe661ada7aba976
ysera34 <ycyoon34@gmail.com>
2019-02-02 10:49:57 +0900

    chapter05 Anonymous Functions and the Function Type: function prefix 'inline'

--- chapter05/Sandbox/src/SimVillage.kt
+++ chapter05/Sandbox/src/SimVillage.kt
@@ -82,7 +82,7 @@ fun main(args: Array<String>) {
     }
 }
 
-fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
     val buildingsNumber = (1..3).shuffled().last()
     println(greetingFunction(playerName, buildingsNumber))
 }
