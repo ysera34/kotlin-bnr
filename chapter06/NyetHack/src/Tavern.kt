69796c6f3c93d6aec1190c1f28c4ba929d0f23a5
ysera34 <ycyoon34@gmail.com>
2019-02-02 13:29:43 +0900

    chapter06 Null Safety and Exceptions: assertion operator

--- chapter06/NyetHack/src/Tavern.kt
+++ chapter06/NyetHack/src/Tavern.kt
@@ -2,15 +2,20 @@ fun main(args: Array<String>) {
     var signatureDrink = "beer"
 //    signatureDrink = null // Error: Null can not be a value of a non-null type String
 
-    var beverage = readLine()
+    var beverage1 = readLine()
 //    beverage = null
 //    beverage?.capitalize()
-    beverage = readLine()?.let {
+    beverage1 = readLine()?.let {
         if (it.isNotBlank()) {
             it.capitalize()
         } else {
             "Beer"
         }
     }
-    println(beverage)
+    println(beverage1)
+
+//    var beverage2 = readLine()
+//    beverage2 = null
+//    beverage2!!.capitalize() // Runtime Error: Exception in thread "main" kotlin.KotlinNullPointerException
+    var beverage2 = readLine()!!.capitalize()
 }
