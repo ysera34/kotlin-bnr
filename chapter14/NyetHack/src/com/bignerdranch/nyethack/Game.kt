3fce2f894a302230d57edeb8c33bd620479d696d
ysera34 <ycyoon34@gmail.com>
2019-02-04 18:53:47 +0900

    Chapter14 Inheritance: initialize

---
+++ chapter14/NyetHack/src/com/bignerdranch/nyethack/Game.kt
@@ -0,0 +1,39 @@
+package com.bignerdranch.nyethack
+
+fun main(args: Array<String>) {
+    val player = Player("Madrigal", 89, true, false).apply {
+        castFireball(5)
+        castFireball()
+    }
+    val secondaryPlayer = Player("Kar")
+    println("${secondaryPlayer.name}'s healthPoints: ${secondaryPlayer.healthPoints}")
+
+    printPlayerStatus(player)
+
+    performCombat()
+    performCombat("Ulrich")
+    performCombat("Hildr", true)
+}
+
+private fun printPlayerStatus(player: Player) {
+    println(
+        "(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+    )
+    println("${player.name} ${player.formatHealthStatus()}")
+}
+
+fun performCombat() {
+    println("There is no enemy.")
+}
+
+fun performCombat(enemyName: String) {
+    println("Begins battle with the $enemyName")
+}
+
+fun performCombat(enemyName: String, isBlessed: Boolean) {
+    if (isBlessed) {
+        println("Begins battle with the $enemyName. Blessed.")
+    } else {
+        println("Begins battle with the $enemyName")
+    }
+}
---
+++ chapter14/NyetHack/src/com/bignerdranch/nyethack/Player.kt
@@ -0,0 +1,80 @@
+package com.bignerdranch.nyethack
+
+import java.io.File
+
+/**
+ * primary constructor
+ */
+class Player(
+    _name: String,
+    /*_healthPoints*/var healthPoints: Int = 100,
+    /*_isBlessed*/val isBlessed: Boolean,
+    /*_isImmortal*/private val isImmortal: Boolean
+) {
+
+    /**
+     * If you are using a custom getter setter,
+     * you must define class properties
+     * and constructor parameter that use temporary variables.
+     */
+    var name = _name
+        get() = "${field.capitalize()} of $hometown"
+        set(value) {
+            field = value.trim()
+        }
+
+    /*val hometown: String = selectHometown()*/
+    val hometown by lazy { selectHometown() }
+
+    /**
+     * Properties that use automatically generated default getters and setters
+     * can be defined only in the primary constructor, not in the class itself.
+     */
+    /*var healthPoints = _healthPoints*/
+    /*val isBlessed = _isBlessed*/
+    /*private val isImmortal = _isImmortal*/
+
+    /**
+     * initializer block, check property value
+     */
+    init {
+        require(healthPoints > 0) { "healthPoints must be greater than zero." }
+        require(name.isNotBlank()) { "player must have a name." }
+    }
+
+    /**
+     * secondary constructor
+     */
+    constructor(name: String) : this(
+        name,
+        /*healthPoints = 100,*/ // using default value of primary constructor
+        isBlessed = true,
+        isImmortal = false
+    ) {
+        if (name.toLowerCase() == "kar") healthPoints = 40
+    }
+
+    private fun selectHometown() = File("data/towns.txt")
+        .readText()
+        .split("\n")
+        .shuffled()
+        .first()
+
+    fun castFireball(numFireballs: Int = 2) = // default argument
+        println("A bunch of fireballs appear. (x$numFireballs)")
+
+    fun formatHealthStatus() =
+        when (healthPoints) {
+            100 -> "is best state"
+            in 90..99 -> "has only a few abrasions."
+            in 75..89 -> if (isBlessed) {
+                "had minor injuries but soon healed."
+            } else {
+                "has only minor injuries."
+            }
+            in 15..74 -> "seems to be hurt a lot."
+            else -> "is worst state"
+        }
+
+    fun auraColor() = if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
+}
