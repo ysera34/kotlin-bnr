aa3279eee5d21ef11a6d48c8577d2bac1b81bc67
ysera34 <ycyoon34@gmail.com>
2019-02-06 17:27:37 +0900

    Chapter16 Interfaces and Abstract Classes: abstract class declaration and implementation

--- chapter16/NyetHack/src/com/bignerdranch/nyethack/Creature.kt
+++ chapter16/NyetHack/src/com/bignerdranch/nyethack/Creature.kt
@@ -13,3 +13,25 @@ interface Fightable {
 
     fun attack(opponent: Fightable): Int
 }
+
+abstract class Monster(
+    val name: String,
+    val description: String,
+    override var healthPoints: Int
+) : Fightable {
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = damageRoll
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+}
+
+class Goblin(
+    name: String = "Goblin",
+    description: String = "ugly goblin.",
+    healthPoints: Int = 30
+) : Monster(name, description, healthPoints) {
+
+    override val diceCount = 2
+    override val diceSides = 8
+}
