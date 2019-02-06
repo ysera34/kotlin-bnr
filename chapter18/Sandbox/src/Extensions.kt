c62a701d50a0b9c0f19f1042911049e8cf7fd85b
ysera34 <ycyoon34@gmail.com>
2019-02-06 20:11:23 +0900

    Chapter18 Extensions: property extension

--- chapter18/Sandbox/src/Extensions.kt
+++ chapter18/Sandbox/src/Extensions.kt
@@ -1,3 +1,11 @@
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
+
+/**
+ * Extension property cannot be initialized because it has no backing field
+ */
+//var String.preferredCharacters = 10
+
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
 //fun Any.easyPrint() = println(this)
@@ -12,4 +20,6 @@ fun main(args: Array<String>) {
     "Madrigal came out of the building".addEnthusiasm().easyPrint()
     "Madrigal came out of the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
+
+    "How many vowels?".numVowels.easyPrint()
 }
