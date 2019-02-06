3d990f51590d7b1ef7e6e57ea9bce0945dea669d
ysera34 <ycyoon34@gmail.com>
2019-02-06 13:20:03 +0900

    Chapter15 Objects: enum class

--- chapter15/NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
+++ chapter15/NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
@@ -3,3 +3,21 @@ package com.bignerdranch.nyethack
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
 }
+
+enum class Direction(private val coordinate: Coordinate) {
+    NORTH(Coordinate(0, -1)),
+    EAST(Coordinate(1, 0)),
+    SOUTH(Coordinate(0, 1)),
+    WEST(Coordinate(-1, 0));
+
+    fun getDirectionName(direction: Direction) =
+        when (direction) {
+            NORTH -> "North"
+            EAST -> "East"
+            SOUTH -> "South"
+            WEST -> "West"
+        }
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+        Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+}
