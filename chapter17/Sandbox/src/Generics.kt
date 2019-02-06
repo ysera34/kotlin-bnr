06e3106186c79cdb371dd5fa68b3fd1a356f734e
ysera34 <ycyoon34@gmail.com>
2019-02-06 18:35:49 +0900

    Chapter17 Generics: generic function

--- chapter17/Sandbox/src/Generics.kt
+++ chapter17/Sandbox/src/Generics.kt
@@ -1,5 +1,14 @@
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
+
+    fun <R> fetch(lootModFunction: (T) -> R): R? {
+        return lootModFunction(loot).takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -9,4 +18,14 @@ class Coin(val value: Int)
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("A normal fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("Pick $name from the LootBox")
+    }
+
+    val coin = lootBoxOne.fetch {
+        Coin(it.value * 3)
+    }
+    coin?.let { println(it.value) }
 }
