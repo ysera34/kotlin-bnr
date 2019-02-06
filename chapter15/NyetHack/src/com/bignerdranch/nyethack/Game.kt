de1f4d6fad620b668a551ee3c6d824baf65a2fe3
ysera34 <ycyoon34@gmail.com>
2019-02-06 12:29:17 +0900

    Chapter15 Objects: object declaration

--- chapter15/NyetHack/src/com/bignerdranch/nyethack/Game.kt
+++ chapter15/NyetHack/src/com/bignerdranch/nyethack/Game.kt
@@ -1,20 +1,45 @@
 package com.bignerdranch.nyethack
 
-fun main(args: Array<String>) {
-    val player = Player("Madrigal").apply {
-        castFireball()
-    }
-
-    var currentRoom = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    printPlayerStatus(player)
+/**
+ * object expression
+ */
+val abandonedTownSquare = object : TownSquare() {
+    override fun load() = "I expected you to be welcome, but nobody here."
 }
 
-private fun printPlayerStatus(player: Player) {
-    println(
-        "(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})"
-    )
-    println("${player.name} ${player.formatHealthStatus()}")
+fun main(args: Array<String>) {
+
+    Game.play()
+}
+
+/**
+ * object declaration
+ */
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom: Room = TownSquare()
+
+    init {
+        println("Welcome to visit.")
+        player.castFireball()
+    }
+
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Please enter a command: ")
+            println("Recent commands: ${readLine()}")
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println(
+            "(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+        )
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
 }
--- chapter15/NyetHack/src/com/bignerdranch/nyethack/Room.kt
+++ chapter15/NyetHack/src/com/bignerdranch/nyethack/Room.kt
@@ -12,7 +12,7 @@ open class TownSquare : Room("Town Square") {
     override val dangerLevel = super.dangerLevel - 3
     private var bellSound = "ding-dong"
 
-    final override fun load() = "Your participation is welcomed by all residents!\n${ringBell()}"
+    override fun load() = "Your participation is welcomed by all residents!\n${ringBell()}"
 
     private fun ringBell() = "Inform your arrival at the bell tower. $bellSound"
 }
