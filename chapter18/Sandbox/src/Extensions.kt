5aaaf15a08149ef88e7245176bca28fb03acfedf
ysera34 <ycyoon34@gmail.com>
2019-02-06 20:04:21 +0900

    Chapter18 Extensions: generic function extension

--- chapter18/Sandbox/src/Extensions.kt
+++ chapter18/Sandbox/src/Extensions.kt
@@ -1,9 +1,15 @@
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+//fun Any.easyPrint() = println(this)
+//fun Any.easyPrint(): Any {
+fun <T> T.easyPrint(): T {
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
     println("Madrigal came out of the building".addEnthusiasm())
     "Madrigal came out of the building".addEnthusiasm().easyPrint()
+    "Madrigal came out of the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
 }
