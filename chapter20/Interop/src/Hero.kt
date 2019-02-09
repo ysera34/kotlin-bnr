3bc3f56cb6f970fe3118dc0618ea99379693d0cd
ysera34 <ycyoon34@gmail.com>
2019-02-09 19:56:51 +0900

    Chapter20 Java Interoperability: type mapping

--- chapter20/Interop/src/Hero.kt
+++ chapter20/Interop/src/Hero.kt
@@ -4,4 +4,8 @@ fun main(args: Array<String>) {
 
     val friendshipLevel = adversary.determineFriendshipLevel()
     println(friendshipLevel?.toLowerCase() ?: "I do not know what it means.")
+
+    val adversaryHitPoints: Int = adversary.hitPoints
+    println(adversaryHitPoints.dec())
+    println(adversaryHitPoints.javaClass)
 }
--- chapter20/Interop/src/Jhava.java
+++ chapter20/Interop/src/Jhava.java
@@ -3,6 +3,8 @@ import org.jetbrains.annotations.Nullable;
 
 public class Jhava {
 
+    public int hitPoints = 52489112;
+
     @NotNull
     public String utterGreeting() {
         return "BLARGH";
