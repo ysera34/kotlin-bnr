1bb85dd66ea279cc993ba74713d62161572f2f3f
ysera34 <ycyoon34@gmail.com>
2019-02-09 19:50:36 +0900

    Chapter20 Java Interoperability: null handling, platform type and null relevant annotation

--- chapter20/Interop/src/Hero.kt
+++ chapter20/Interop/src/Hero.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
     val adversary = Jhava()
     println(adversary.utterGreeting())
+
+    val friendshipLevel = adversary.determineFriendshipLevel()
+    println(friendshipLevel?.toLowerCase() ?: "I do not know what it means.")
 }
--- chapter20/Interop/src/Jhava.java
+++ chapter20/Interop/src/Jhava.java
@@ -1,5 +1,15 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Jhava {
+
+    @NotNull
     public String utterGreeting() {
         return "BLARGH";
+    }
+
+    @Nullable
+    public String determineFriendshipLevel() {
+        return null;
     }
 }
