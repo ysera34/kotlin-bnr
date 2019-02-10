a2ff8caed9ad9cc970f95410fad39fae29114b57
ysera34 <ycyoon34@gmail.com>
2019-02-11 00:17:15 +0900

    Chapter22 Introduction to Coroutines: initialize

---
+++ chapter22/Samodelkin/app/src/main/java/android/bignerdranch/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,43 @@
+package android.bignerdranch.samodelkin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new_character.*
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
+class NewCharacterActivity : AppCompatActivity() {
+    private lateinit var characterData: CharacterGenerator.CharacterData
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+
+        characterData = savedInstanceState?.characterData ?: CharacterGenerator.generate()
+
+        generateButton.setOnClickListener {
+            characterData = CharacterGenerator.generate()
+            displayCharacterData()
+        }
+        displayCharacterData()
+    }
+
+    private fun displayCharacterData() {
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.characterData = characterData
+    }
+}
