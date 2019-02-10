3f026c41edd004d232fcc9047a3c196e75efaf0a
ysera34 <ycyoon34@gmail.com>
2019-02-10 20:30:33 +0900

    Chapter21 Building Your First Android App with Kotlin: initialize

---
+++ chapter21/Samodelkin/app/src/main/java/android/bignerdranch/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,12 @@
+package android.bignerdranch.samodelkin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCharacterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+    }
+}
