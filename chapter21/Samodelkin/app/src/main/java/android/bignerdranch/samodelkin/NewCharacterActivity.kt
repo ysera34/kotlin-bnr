a5701b244faf021ce9e27d19d53ca9028dae52ec
ysera34 <ycyoon34@gmail.com>
2019-02-10 23:06:02 +0900

    Chapter21 Building Your First Android App with Kotlin: code refactoring using the kotlin extension

--- chapter21/Samodelkin/app/src/main/java/android/bignerdranch/samodelkin/NewCharacterActivity.kt
+++ chapter21/Samodelkin/app/src/main/java/android/bignerdranch/samodelkin/NewCharacterActivity.kt
@@ -1,9 +1,8 @@
 package android.bignerdranch.samodelkin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new_character.*
 
 class NewCharacterActivity : AppCompatActivity() {
     private var characterData = CharacterGenerator.generate()
@@ -11,14 +10,10 @@ class NewCharacterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
+        displayCharacterData()
+    }
 
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val raceTextView = findViewById<TextView>(R.id.raceTextView)
-        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
-        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
-        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
-        val generateButton = findViewById<Button>(R.id.generateButton)
-
+    private fun displayCharacterData() {
         characterData.run {
             nameTextView.text = name
             raceTextView.text = race
